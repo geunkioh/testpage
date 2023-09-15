034dfecde869365e3d1edf24b13c60152bdc2c66
Kunki Oh <kunkiya@gmail.com>
2023-09-16 02:46:56 +0900

    try

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-if (/instagram/i.test(navigator.userAgent) || navigator.userAgent === undefined) {
-    window.open("https://naver.com", "_blank");
-} else {
-    if (window.opener) {
-        window.opener.postMessage("https://naver.com", '*');
-        window.self.close();
+try {
+    if (/instagram/i.test(navigator.userAgent) || navigator.userAgent === undefined) {
+        alert("This is inside instagram. It will go to instagram")
+        window.open("https://naver.com", "_blank");
     } else {
-        alert("Window Opener is undefined")
+        alert("No instagram")
+        if (window.opener) {
+            alert("There is opener")
+            window.opener.postMessage("https://naver.com", '*');
+            window.self.close();
+        } else {
+            alert("Window Opener is undefined")
+            window.open("https://naver.com", "_blank");
+        }
     }
+}
+catch (e) {
+    console.log(e)
 }
